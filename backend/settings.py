a1b5dd1dd28a5e21c349983f4f74d355723d8674
Tony-Omondi <otienotony598@gmail.com>
2025-08-29 17:43:42 +0300

    python manage.py collectstatic

--- backend/settings.py
+++ backend/settings.py
@@ -150,12 +150,12 @@ SIMPLE_JWT = {
 }
 
 # SMTP Email config
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"  # Changed to console for debugging
-EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
-EMAIL_PORT = os.getenv("EMAIL_PORT", 587)
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "nitonito598@gmail.com"
-EMAIL_HOST_PASSWORD = "ouuippiqxvunrffy"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")  # or AWS SES host
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "nitonito598@gmail.com")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "noreply@example.com")
 
 # Paystack settings
